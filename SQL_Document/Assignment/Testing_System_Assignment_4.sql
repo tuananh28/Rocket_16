7ad4407f4900fe264c7ecedee006a16f149c40dc
tuananh28 <56882009+tuananh28@users.noreply.github.com>
2021-07-02 21:00:21 +0700

    Homework

--- SQL_Document/Assignment/Testing_System_Assignment_4.sql
+++ SQL_Document/Assignment/Testing_System_Assignment_4.sql
@@ -46,13 +46,88 @@ ON			CQ.CategoryID = Q.CategoryID
 GROUP BY	Q.Content;
 
 -- Question 7: Thông kê mỗi Question được sử dụng trong bao nhiêu Exam
+
+SELECT 		EQ.*, COUNT(EQ.QuestionID) AS 'SO LUONG'
+FROM		`Question` Q
+JOIN		`ExamQuestion` EQ
+ON			Q.QuestionID = EQ.QuestionID
+GROUP BY	EQ.QuestionID
+HAVING		COUNT(EQ.QuestionID);
+
 -- Question 8: Lấy ra Question có nhiều câu trả lời nhất
+ SELECT		A.*,COUNT(A.QuestionID) AS 'SO LUONG'
+ FROM		`Question` Q
+ JOIN		`Answer` A
+ ON			Q.QuestionID = A.QuestionID
+ GROUP BY	A.Content
+ HAVING		COUNT(A.QuestionID) = (SELECT MAX(COUNTQ)
+									FROM (SELECT		COUNT(A.QuestionID) AS COUNTQ
+											FROM		`Question` Q
+											JOIN		`Answer` A
+											ON			Q.QuestionID = A.QuestionID
+                                            GROUP BY	A.Content)AS MaxQuestion);
+
 -- Question 9: Thống kê số lượng account trong mỗi group 
+SELECT 	G.*, COUNT(GA.GroupID) AS 'SO LUONG ACCOUNT'
+FROM	`Group` G
+JOIN	`GroupAccount` GA
+ON 		G.GroupID =  GA.GroupID
+GROUP BY	GA.GroupID;
+
 -- Question 10: Tìm chức vụ có ít người nhất
+
+SELECT 		P.*, COUNT(A.PositionID)
+FROM		`Position` P
+JOIN		`Account` A
+ON			P.PositionID = A.PositionID
+GROUP BY	A.PositionID
+HAVING		COUNT(A.PositionID) = (SELECT MIN(COUNTP)
+									FROM	(SELECT COUNT(A.PositionID) AS COUNTP
+											FROM		`Position` P
+											JOIN		`Account` A
+											ON			P.PositionID = A.PositionID
+											GROUP BY	A.PositionID)AS MinPosition);	
 -- Question 11: Thống kê mỗi phòng ban có bao nhiêu dev, test, scrum master, PM
--- Question 12: Lấy thông tin chi tiết của câu hỏi bao gồm: thông tin cơ bản của 
--- question, loại câu hỏi, ai là người tạo ra câu hỏi, câu trả lời là gì, …
+SELECT P.*,COUNT(A.PositionID) AS 'SO LUONG'
+FROM	`Account` A
+JOIN	`Position` P
+ON		P.PositionID = A.PositionID
+GROUP BY	A.PositionID;
+/* Question 12: Lấy thông tin chi tiết của câu hỏi bao gồm: thông tin cơ bản của
+  question, loại câu hỏi, ai là người tạo ra câu hỏi, câu trả lời là gì, …*/
+
+SELECT	*
+FROM	`Question` Q
+JOIN	`Answer` A
+ON		Q.QuestionID = A.QuestionID;
+
+
 -- Question 13: Lấy ra số lượng câu hỏi của mỗi loại tự luận hay trắc nghiệm
+
+SELECT 		TQ.*, COUNT(Q.TypeID) AS ' Số Lượng'
+FROM		`TypeQuestion` TQ
+JOIN		`Question` Q
+ON 			TQ.TypeID = Q.TypeID
+GROUP BY 	Q.TypeID;	
+
 -- Question 14:Lấy ra group không có account nào
+
+SELECT		*
+FROM		`Group` G
+LEFT JOIN	`GroupAccount` GA
+ON			G.GroupID = GA.GroupID
+WHERE		GA.GroupID IS NULL;
 -- Question 15: Lấy ra group không có account nào
+
+SELECT 	*
+FROM	`Group`
+WHERE	GroupID NOT IN (SELECT GroupID
+						FROM	`GroupAccount` );
+
 -- Question 16: Lấy ra question không có answer nào
+
+SELECT	*
+FROM	`Question` Q
+LEFT JOIN	`Answer` A
+ON 		Q.QuestionID =  A.QuestionID
+WHERE	A.QuestionID IS NULL;
