2b9e131f1a2f7bf8c27f07118690d0364196bcfe
tuananh28 <56882009+tuananh28@users.noreply.github.com>
2021-07-16 02:25:54 +0700

    Commit

---
+++ Java_Document/Testing System 4/src/com/vti/frontend/Frontend_Constructor.java
@@ -0,0 +1,12 @@
+package com.vti.frontend;
+
+import com.vti.backend.Ex1_Constructor;
+
+public class Frontend_Constructor {
+	public static void main(String[] args) {
+		Ex1_Constructor ex1 = new Ex1_Constructor();
+		ex1.Question1();
+		ex1.Question2();
+		ex1.Question3();
+	}
+}
---
+++ Java_Document/Testing System 4/src/com/vti/frontend/Frontend_Encapsulation.java
@@ -0,0 +1,10 @@
+package com.vti.frontend;
+
+import com.vti.backend.Ex4_Encapsulation;
+
+public class Frontend_Encapsulation {
+	public static void main(String[] args) {
+		Ex4_Encapsulation ex4 = new Ex4_Encapsulation();
+		ex4.Question1();
+ 	}
+}
