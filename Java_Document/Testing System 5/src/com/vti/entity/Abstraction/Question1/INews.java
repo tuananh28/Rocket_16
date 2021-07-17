d8cabe69ef3be992fc19591f4cb4a504b3be85f1
tuananh28 <56882009+tuananh28@users.noreply.github.com>
2021-07-17 23:16:35 +0700

    Commit

---
+++ Java_Document/Testing System 5/src/com/vti/entity/Abstraction/Question1/INews.java
@@ -0,0 +1,8 @@
+package com.vti.entity.Abstraction.Question1;
+
+
+public interface INews {
+	void Display();
+	float Calculate();
+	
+}
---
+++ Java_Document/Testing System 5/src/com/vti/frontend/Frontend_Ex1.java
@@ -0,0 +1,10 @@
+package com.vti.frontend;
+
+import com.vti.backend.Ex1_Abstraction;
+
+public class Frontend_Ex1 {
+	public static void main(String[] args) {
+		Ex1_Abstraction ex1 = new Ex1_Abstraction();
+		ex1.Question1();
+	}
+}
