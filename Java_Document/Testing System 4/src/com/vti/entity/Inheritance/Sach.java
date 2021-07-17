86889ed0ad64f13e41caeb7f090b4b40efeb5816
tuananh28 <56882009+tuananh28@users.noreply.github.com>
2021-07-17 11:31:27 +0700

    Commit

---
+++ Java_Document/Testing System 4/src/com/vti/entity/Inheritance/Sach.java
@@ -0,0 +1,35 @@
+package com.vti.entity.Inheritance;
+
+import java.util.Scanner;
+
+public class Sach extends TaiLieu{
+	String TenTG;
+	int SoTrang;
+	private Scanner scanner;
+	public String getTenTG() {
+		return TenTG;
+	}
+	public void setTenTG(String tenTG) {
+		TenTG = tenTG;
+	}
+	public int getSoTrang() {
+		return SoTrang;
+	}
+	public void setSoTrang(int soTrang) {
+		SoTrang = soTrang;
+	}
+	
+	public void Input() {
+		super.Input();
+		scanner = new Scanner(System.in);
+		System.out.print("Tên Tác Giả : ");
+		TenTG = scanner.nextLine();
+		System.out.print("Số trang : ");
+		SoTrang =  Integer.parseInt(scanner.nextLine());
+	}
+	public void Output() {
+		super.Output();
+		System.out.println("Tên Tác Giả : "+TenTG);
+		System.out.println("Số trang : "+SoTrang);
+	}
+}
---
+++ Java_Document/Testing System 4/src/com/vti/frontend/Frontend_Abstraction.java
@@ -0,0 +1,13 @@
+package com.vti.frontend;
+
+import com.vti.backend.Ex6_Abstraction;
+
+public class Frontend_Abstraction {
+
+	public static void main(String[] args) {
+		Ex6_Abstraction ex6 = new Ex6_Abstraction();
+		ex6.Question1();
+
+	}
+
+}
