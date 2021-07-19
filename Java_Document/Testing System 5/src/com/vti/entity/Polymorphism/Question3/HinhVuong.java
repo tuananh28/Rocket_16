b65c32ae2b99bbf71583e598e7a342ee75a7e9bc
tuananh28 <56882009+tuananhbuii@users.noreply.github.com>
2021-07-19 15:37:13 +0700

    Commit

---
+++ Java_Document/Testing System 5/src/com/vti/entity/Polymorphism/Question3/HinhVuong.java
@@ -0,0 +1,9 @@
+package com.vti.entity.Polymorphism.Question3;
+
+
+public class HinhVuong extends HinhChuNhat {
+	public HinhVuong(Float canha) {
+		super(canha, canha);
+		// TODO Auto-generated constructor stub
+	}
+}
