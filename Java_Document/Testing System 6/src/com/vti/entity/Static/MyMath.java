ae1a17b8b0dfc1f03eb856f7c1106ae33956f0e0
tuananh28 <56882009+tuananhbuii@users.noreply.github.com>
2021-07-19 22:20:39 +0700

    Commit

---
+++ Java_Document/Testing System 6/src/com/vti/entity/Static/MyMath.java
@@ -0,0 +1,23 @@
+package com.vti.entity.Static;
+
+public class MyMath {
+	public static int max(int a, int b) {
+		if (a <= b) {
+			return b;
+		} else {
+			return a;
+		}
+	}
+
+	public static int min(int a, int b) {
+		if (a <= b) {
+			return a;
+		} else {
+			return b;
+		}
+	}
+
+	public static int sum(int a, int b) {
+		return a + b;
+	}
+}
